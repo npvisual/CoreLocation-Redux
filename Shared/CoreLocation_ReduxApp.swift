91bfc510536e20d2a5bcdcd95c87f51bb3bbb956
Nick <npvisual@users.noreply.github.com>
2020-09-14 16:53:25 -0500

    Initial Commit

---
+++ Shared/CoreLocation_ReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreLocation_ReduxApp.swift
+//  Shared
+//
+//  Created by Nicolas Philippe on 9/14/20.
+//
+
+import SwiftUI
+
+@main
+struct CoreLocation_ReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
